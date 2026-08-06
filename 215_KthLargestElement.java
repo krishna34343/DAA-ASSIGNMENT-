737caf0f2a0c0555d6b1e093cad8e3dcfc0e27eb
krishna34343 <krishnataili678@gmail.com>
2026-08-06 08:36:26 +0530

    Delete 215_KthLargestElement.java

--- 215_KthLargestElement.java
+++
@@ -1,8 +0,0 @@
-import java.util.PriorityQueue;
-class Solution{
- public int findKthLargest(int[] nums,int k){
-  PriorityQueue<Integer> pq=new PriorityQueue<>();
-  for(int x:nums){pq.offer(x); if(pq.size()>k)pq.poll();}
-  return pq.peek();
- }
-}
